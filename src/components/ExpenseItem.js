e54d00e71fa738d3b2339458d2f6447b4ebf6652
Harjeevan Tanda <harjeevan.tanda@gmail.com>
2022-07-18 17:36:57 -0400

    custom components completed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate.js";
+import Card from "./Card.js";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', {month:'long'})
-  const day = props.date.toLocaleString('en-US', {day:'2-digit'})
-  const year = props.date.getFullYear('en-US', {})
   return (
-    <div className="expense-item">
-      <div>{month + ' ' + day + ', ' + year}</div>
-      <div className="expense-item_descrpition">
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__descrpition">
         <h2>{props.title}</h2>
-        <div className="expense-item_price">{props.amount}</div>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
