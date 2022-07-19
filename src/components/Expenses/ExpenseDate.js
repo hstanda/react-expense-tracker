67074acffac8f011c1a90745126293e9a61f6911
Harjeevan Tanda <harjeevan.tanda@gmail.com>
2022-07-18 20:00:11 -0400

    Component tree and ES6

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,4 +1,4 @@
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear("en-US", {});
